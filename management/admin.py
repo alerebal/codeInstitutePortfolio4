bf9cbd8db6ff2e9f7a8a37b079498efb03304e04
Alejandro Rebaldería <ale.rebal@gmail.com>
2022-04-29 07:43:36 +0000

    Add Reservation model and add admin to it

--- management/admin.py
+++ management/admin.py
@@ -1,3 +1,9 @@
+""" Admin """
 from django.contrib import admin
+from .models import Reservation
 
-# Register your models here.
+
+@admin.register(Reservation)
+class ReservationAdmin(admin.ModelAdmin):
+    """ Reservation Admin class"""
+    list_filter = ('name', 'email')
--- management/models.py
+++ management/models.py
@@ -1,3 +1,23 @@
+""" Models """
 from django.db import models
+from django.core.validators import MaxValueValidator, MinValueValidator
+from .helpers import lunch, dinner
 
-# Create your models here.
+
+class Reservation(models.Model):
+    """Reservation Cale"""
+    guests = models.PositiveIntegerField(default=1,
+                                         validators=[MinValueValidator(1),
+                                                     MaxValueValidator(10)])
+    time_options = lunch + dinner
+    date = models.DateField()
+    time = models.CharField(max_length=30,
+                            choices=time_options,
+                            default='12:00')
+    name = models.CharField(max_length=30)
+    last_name = models.CharField(max_length=30)
+    email = models.EmailField()
+    phone = models.CharField(max_length=10, blank=True)
+
+    def __str__(self):
+        return f"{self.name} {self.last_name} - {self.date} - {self.guests}"
