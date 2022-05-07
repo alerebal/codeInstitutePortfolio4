9c332c06ca67abc1f286fa48f79911505f8e07ee
Alejandro Rebaldería <ale.rebal@gmail.com>
2022-05-07 06:28:40 +0000

    Add functionality to avoid a reservation if there is not room available
    If no room available the user cannot make a reservation at the time they wanted to do it.
    Some refactioring has been done

--- management/views.py
+++ management/views.py
@@ -13,58 +13,61 @@ def reservation_form_view(request):
     """ Reservation form view """
     if request.method == 'POST':
         form = ReservationForm(request.POST)
-        name = form.data['name']
-        date = form.data['date']
-        time = form.data['time']
-        user_data = {
-            'name': name,
-            'date': date,
-            'time': time
-        }
         diff_hour = False
-        reservation_id = None
+        reservation = None
+        no_room = False
         try:
             resers = Reservation.objects.filter(email=request.POST['email'])
-            if len(resers) > 0:
-                # User has already reservations
-                for reser in resers:
-                    if str(reser.date) == str(request.POST['date']):
-                        # User has a reservation at the same day and same time
-                        if str(reser.time) == str(request.POST['time']):
-                            reservation_id = reser.pk
-                            raise ValueError('same day and time')
-                        else:
-                            # same day, different time
-                            reservation_id = reser.pk
-                            diff_hour = True
-                            raise ValueError('same day but different time')
-                # different day, the reservation can be saved
-                if form.is_valid():
-                    form.save()
-                    context = {
-                        'form': form,
-                        'user_res': user_data,
-                        'reservation_id': reservation_id
-                    }
-                return render(request, 'reservation_msg.html', context)
+            if not is_room_available(request.POST['date'],
+                                     request.POST['time'],
+                                     request.POST['guests']):
+                no_room = True
+                context = {
+                    'form': form,
+                    'no_room': no_room
+                }
+                return render(request, 'reservation_form.html', context)
             else:
-                # not reservations, reservation can be saved
-                form = ReservationForm(request.POST)
-                if form.is_valid():
-                    form.save()
-                    context = {
-                        'form': form,
-                        'user_res': user_data,
-                        'reservation_id': reservation_id
-                    }
-                return render(request, 'reservation_msg.html', context)
+                if len(resers) > 0:
+                    # User has already reservations
+                    for reser in resers:
+                        if str(reser.date) == str(request.POST['date']):
+                            # same day, different time
+                            if str(reser.time) != str(request.POST['time']):
+                                diff_hour = True
+                            # same day and same time
+                            reservation = reser
+                            raise ValueError(f'{reser.date} at {reser.time}')
+                    # different day, the reservation can be saved
+                    if form.is_valid():
+                        form.save()
+                        reservations = Reservation.objects.filter(
+                                       email=request.POST['email'])
+                        reservation = reservations[len(reservations)-1]
+                        context = {
+                            'user_res': reservation,
+                            'reservation_id': reservation.id
+                        }
+                    return render(request, 'reservation_msg.html', context)
+                else:
+                    # not reservations, reservation can be saved
+                    form = ReservationForm(request.POST)
+                    if form.is_valid():
+                        form.save()
+                        reservations = Reservation.objects.filter(
+                                       email=request.POST['email'])
+                        reservation = reservations[len(reservations)-1]
+                        context = {
+                            'user_res': reservation,
+                            'reservation_id': reservation.id
+                        }
+                    return render(request, 'reservation_msg.html', context)
         except ValueError as error:
             context = {
-                'error': f'you already have a reservation at {error}',
-                'form': form,
-                'user': user_data,
+                'error': f'you already have a reservation on {error}',
+                'user': reservation or None,
                 'diff_hour': diff_hour,
-                'reservation_id': reservation_id
+                'reservation_id': reservation.pk or None
             }
             return render(request, 'reservation_msg.html', context)
     form = ReservationForm()
@@ -74,12 +77,20 @@ def reservation_form_view(request):
     return render(request, 'reservation_form.html', context)
 
 
+def is_room_available(date, time, guests, room=30):
+    """ Checking for room availability, default 30 people is full room """
+    reservations = Reservation.objects.filter(date=date, time=time)
+    total_people = 0
+    for res in reservations:
+        total_people += res.guests
+    total_people += int(guests)
+    return total_people < room
+
+
 def edit_reservation(request, reservation_id):
     """ Form to edit the reservation"""
-    print(reservation_id)
     reservation = get_object_or_404(Reservation, id=reservation_id)
     if request.method == 'POST':
-        print('post')
         form = ReservationForm(request.POST, instance=reservation)
         if form.is_valid():
             form.save()
